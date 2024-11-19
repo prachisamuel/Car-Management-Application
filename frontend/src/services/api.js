bb57600d4471406cee5514e831122441b8e6fc4a
Prachi Samuel <prachisamuel28@gmail.com>
2024-11-19 05:37:21 +0530

    updated

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: window.location.origin,
+  baseURL: `${window.location.origin}/api`,
   headers: {
     'Content-Type': 'application/json',
   },
