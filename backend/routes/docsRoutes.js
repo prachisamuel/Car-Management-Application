a48e037dff09c4eb2d6c1fb629586409c22c2883
Prachi Samuel <137616292+prachisamuel@users.noreply.github.com>
2024-11-20 02:59:03 +0530

    Update docsRoutes.js

--- backend/routes/docsRoutes.js
+++ backend/routes/docsRoutes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 // API documentation route
 router.get('/docs', (req, res) => {
-  res.redirect('https://www.postman.com/prachisamuel/workspace/public-workspace/collection/28694301-9583ec24-ef42-4a50-9c13-230a60509659?action=share&creator=28694301'); // Replace with your Postman link
+  res.redirect('https://www.postman.com/prachisamuel/workspace/public-workspace/collection/28694301-f8099ed8-f099-4917-9a39-b55409ad4dac?action=share&creator=28694301'); // Replace with your Postman link
 });
 
 module.exports = router;
